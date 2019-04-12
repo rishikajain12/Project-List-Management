0fab6b5ce0f7ac3d6607e4f5e21e588d14219705
rishikajain12 <43432077+rishikajain12@users.noreply.github.com>
2019-04-12 16:30:04 +0530

    Update ictpms.sql
    Updated database file

--- DATABASE/ictpms.sql
+++ DATABASE/ictpms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2019 at 09:26 AM
+-- Generation Time: Apr 12, 2019 at 12:58 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.24
 
@@ -45,10 +45,9 @@ CREATE TABLE `backup` (
 --
 
 INSERT INTO `backup` (`id`, `Proj_code`, `Project_name`, `Impl_code`, `Impl_status`, `Proj_status`, `Remarks`, `Action_required`, `update_date`) VALUES
-(1, 'Supply of 77 Desktop Computers', 'CAA4', 'CAA4', 'Project complete. All the 7 PCs were delivered and deployments carried out in three months. Twenty PCs were reserved for the DSSER Exam Centre and 57 deployed elsewhere; a distribution list for all the 77 PCs exists for reference purpose. Some of the repl', 'Stalled', '', '', '2014-08-03'),
-(2, 'Supply of IT Spares & Consumab', 'CAA', 'CAA1', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the bank guarantee expires 30th April 2014 (CRANE). Payment is by EFT', 'Running', 'Delay in delivery ', 'Expedient delivery\r\n\r\n', '2014-08-06'),
-(3, 'Supply of IT Spares & Consumab', 'CAA', 'CAA1', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the bank guarantee expires 30th April 2014 (CRANE). Payment is by EFT', 'Running', 'Delay in delivery ', 'Expedient delivery\r\n\r\n', '2014-08-06'),
-(4, 'Supply of IT Spares & Consumab', 'CAA', 'CAA1', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the bank guarantee expires 30th April 2014 (CRANE). Payment is by EFT', 'Running', 'Delay in delivery ', 'Expedient delivery\r\n\r\n', '2014-08-21');
+(5, 'fmpst', '3030', '301', 'editing and database', 'Running', 'good', 'nothing', '2019-04-12'),
+(6, 'fmpst', '3030', '301', 'editing and database', 'Stalled', 'new work', 'nothing', '2019-04-12'),
+(7, 'fmpst', '3030', '301', 'editing and database', 'Stalled', 'new work', 'some changes', '2019-04-12');
 
 -- --------------------------------------------------------
 
@@ -82,9 +81,9 @@ CREATE TABLE `general_information` (
   `T_member2` varchar(255) NOT NULL,
   `T_member3` varchar(255) NOT NULL,
   `T_member4` varchar(255) NOT NULL,
-  `Purpose` text NOT NULL,
+  `T_mem5` varchar(255) NOT NULL,
   `Scope` text NOT NULL,
-  `T_member5` varchar(250) CHARACTER SET utf8 NOT NULL,
+  `Purpose` text CHARACTER SET utf8 NOT NULL,
   `github_url` varchar(2043) NOT NULL,
   `Planned_completion` int(11) NOT NULL,
   `Impl_StartDate` date NOT NULL,
@@ -95,17 +94,12 @@ CREATE TABLE `general_information` (
 -- Dumping data for table `general_information`
 --
 
-INSERT INTO `general_information` (`Proj_code`, `Project_name`, `project_mentor`, `AppearsIn_BussPlan`, `team_leader`, `T_member1`, `T_member2`, `T_member3`, `T_member4`,`T_mem5`, `Scope`,`Purpose`,  `github_url`, `Planned_completion`, `Impl_StartDate`, `Impl_EndDate`) VALUES
-('1001', 'bestest', 'ayush gupta', 'NO', 'divya jain', 'rishika', 'prachi', '', '', 'nothing bruh', '', '', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 3, '2019-04-11', '2019-04-11'),
-('101', 'best', 'ayush gupta', 'NO', 'divya jain', 'rishika', 'virendra', 'prachi', '', 'i3', '', '', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 4, '2019-04-12', '2019-04-12'),
-('121', 'bjhb', 'Capex', 'YES', '0000-00-00', '45444', '', '', '', '', '', '0000-00-00', '', 1, '0000-00-00', '0000-00-00'),
-('313002', 'fmp', 'Capex', 'YES', '2019-04-26', '10.000', '', 'divya', 'rishika jain', '', '', '0000-00-00', '', 1, '0000-00-00', '0000-00-00'),
-('313003', 'fmps', 'Capex', 'YES', '2019-04-25', '10000', 'rishika', '', 'rishika jain', '', '', '0000-00-00', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 1, '0000-00-00', '0000-00-00'),
-('CAA', 'Supply of IT Spares & Consumables (lot 1)', 'Capex', 'YES', '2014-06-10', '$37,733 VAT inclusive', 'Computer Point Ltd', 'Manager Information Technology', 'Dr. James Mubiru', 'To provide spares and tools for both DPHS and FIDS', 'To supply 40 IER cleaning kits together with 10 mini PCs', '2014-02-18', '$37,733 VAT inclusive', 455, '2014-11-12', '2014-11-12'),
-('CAA2', 'Supply of Network Spares (lot 2)', 'Support Programme', 'YES', '2014-02-02', '555800', 'Computech Ltd', 'Manager Information Technology', 'Dr. James Mubiru', 'To provide network spares and tools for the CAA network infrastructure', 'To supply twenty (20) categories of items. The list including quantities appears in the contract', '2014-02-18', '$31,013 VAT inclusive', 493, '2014-07-02', '2014-07-02'),
-('CAA3', 'Supply of IT Spares & Consumables (lot 3)', 'Support Programme', 'YES', '0000-00-00', '5466600', 'Computech Ltd', 'Manager Information Technology', 'Dr. James Mubiru', 'To provide IT spares, consumables and tools to be used in the CAA IT infrastructure', 'To supply nineteen categories of items. The list including quantities, appears in the contract', '2014-02-18', '$31,130 VAT inclusive', 1, '2014-10-22', '2014-10-22'),
-('CAA4', 'Supply of 77 Desktop Computers & UPS', 'Capex', 'YES', '0000-00-00', '555800', 'Computech Ltd', 'Manager Information Technology', '', 'To enhance facilitation for computer users through deployment of desktop computers', 'To deploy 77 desktop computers complete with UPS; processor 3GHz, RAM 4GB, hard disk 500GB, OS MS Windows Prof., 19A??,??,? monitor', '2013-10-21', '$58,193', 1, '2014-09-27', '2014-09-27'),
-('TST1', 'TEST', 'Support Programme', 'YES', '2014-07-29', '46000', 'James', 'JACKSON', 'BRIAN', 'For purpose', 'Scope here', '2014-08-29', '340000', 259, '2014-07-31', '2014-07-31');
+INSERT INTO `general_information` (`Proj_code`, `Project_name`, `project_mentor`, `AppearsIn_BussPlan`, `team_leader`, `T_member1`, `T_member2`, `T_member3`, `T_member4`, `T_mem5`, `Scope`, `Purpose`, `github_url`, `Planned_completion`, `Impl_StartDate`, `Impl_EndDate`) VALUES
+('101', 'HUB', 'ayush gupta', 'YES', 'Prachi', 'rishika', 'virendra', 'divya', 'rishika jain', 'bab', 'scope', 'purpose', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 12, '2019-04-23', '2019-04-23'),
+('3030', 'fmpst', 'ayush gupta', 'NO', 'divya jain', 'rishika', 'virendra', 'prachi', 'babita', 'geeta', '', '', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 1, '2019-04-26', '2019-04-26'),
+('3085', 'web_page', 'ayush gupta', 'NO', 'divya jain', 'rishika', 'virendra', 'prachi', 'babita', 'geeta', 'nothing', 'just for fun', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 20, '2019-04-23', '2019-04-23'),
+('313001', 'fmp', 'ayush gupta', 'NO', 'divya jain', 'rishika', 'virendra', 'prachi', 'rishika jain', 'geeta', 'dfs', 'dqewerat', 'http://localhost/phpmyadmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 1, '2019-04-20', '2019-04-20'),
+('911', 'pmmp', 'aaditya sir', 'NO', 'rishika jain', 'divya', 'virendra', 'prachi', 'babita', 'bab', 'scopeeeeeeeeeee', 'purposeeeeeeeeeeeee', 'http://localhvkjrnkrhgkgkj,jgtfenhjfukjh3rmin/sql.php?server=1&db=ictpms&table=general_information&pos=0', 1, '2019-04-16', '2019-04-16');
 
 -- --------------------------------------------------------
 
@@ -127,10 +121,11 @@ CREATE TABLE `implementation_status` (
 --
 
 INSERT INTO `implementation_status` (`Proj_code`, `Impl_code`, `Impl_status`, `Proj_status`, `Remarks`, `Action_required`) VALUES
-('CAA', 'CAA1', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the bank guarantee expires 30th April 2014 (CRANE). Payment is by EFT', 'Completed', 'Delay in delivery ', 'Expedient delivery\r\n\r\n'),
-('CAA2', 'CAA2', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the performance security is valid up to 31st May 2014 (Diamond Trust Bank). Payment is by EFT', 'Completed', 'Delay in delivery ', 'Expedient delivery'),
-('CAA3', 'CAA3', 'Deliveries are not made yet. Warranty for the items runs for 12 months and the performance security is valid upto 31st May 2014 (Diamond Trust Bank). Payment is by EFT', 'Terminated', 'Delay in delivery ', 'Expedient delivery'),
-('CAA4', 'CAA4', 'Project complete. All the 7 PCs were delivered and deployments carried out in three months. Twenty PCs were reserved for the DSSER Exam Centre and 57 deployed elsewhere; a distribution list for all the 77 PCs exists for reference purpose. Some of the repl', 'Running', '', '');
+('101', '101', 'scenario', 'Running', 'Remarks', 'Action'),
+('3030', '301', 'editing and database', 'Stalled', 'new work', 'xyz'),
+('3085', '101', 'whatever', 'Running', 'no remarks', 'shut up'),
+('313001', '300', 'sada', 'Running', 'sad', 'very sad'),
+('911', '102', 'project scenario', 'Completed', 'no remarks', 'no action');
 
 -- --------------------------------------------------------
 
@@ -153,11 +148,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `name`, `department`, `role`, `email_address`, `username`, `password`) VALUES
-('', 'demo', 'demo', 'demo', 'demo@demo.com', 'demo', 'demo'),
 ('1323', 'rishika jain', 'cse', 'leader', 'rishikajainranu@gmail.com', 'rishika12', 'caf1a3dfb505ffed0d024130f58c5cfa'),
 ('A1', 'Admin', 'ITdepatment', 'MIT', 'mit@caa.co.ug', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-('A1111', 'sinam', 'sinam', 'sinam', 'harona10@yahoo.com', 'sinam', 'd41d8cd98f00b204e9800998ecf8427e'),
-('divya29', 'divya jain', 'cse', 'manager', '1998chahar@gmail.com', 'divya', '202cb962ac59075b964b07152d234b70');
+('divya29', 'divya jain', 'cse', 'manager', '1998chahar@gmail.com', 'divya', '202cb962ac59075b964b07152d234b70'),
+('virendra', 'virendra sharma', 'cse', 'manager', '17etccs011@technonjr.org', 'virendra21', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
@@ -201,7 +195,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `backup`
 --
 ALTER TABLE `backup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
